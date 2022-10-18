ed33627a1777c6448be1a188c6b3d8eda8759aaf
hima707ch <107808488+hima707ch@users.noreply.github.com>
2022-10-18 10:12:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,6 +26,10 @@ app.use("/profile",profile);
 
 if(process.env.NODE_ENV = "production"){
     app.use(express.static("frontend/build"));
+    const path = require("path");
+    app.get("*", (req,res)=>{
+        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
+    })
 }
 
 const PORT = process.env.PORT || 5000;
